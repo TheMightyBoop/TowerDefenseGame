8a6163212a5b64b6052c7867aa5eab8f0b10ae77
TheMightyBoop <43588463+TheMightyBoop@users.noreply.github.com>
2019-05-27 18:50:35 -0500

    Options Menu Added
    Added Options menu with following settings:
    -Resolution dropdown
    -Fullscreen toggle
    -Graphics dropdown
    -Volume slider

---
+++ Tower Defense Game/Assets/OptionsMenuSelect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuSelect : MonoBehaviour {
+
+    public string modeSelect = "ModeSelect";
+
+    public SceneFader sceneFader;
+
+    public void Quit()
+    {
+        sceneFader.FadeTo(modeSelect);
+    }
+}
